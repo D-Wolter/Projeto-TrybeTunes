2d53eacdf7942ef4a46f3c0d8a0adbf1eb0d454a
Daniel Wolter <wolter.daniel@gmail.com>
2022-08-30 18:10:02 -0300

    requisito 2 em andamento

--- src/App.js
+++ src/App.js
@@ -9,10 +9,47 @@ import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userInputName: '',
+      isSaveButtonDisabled: true,
+    };
+  }
+
+  checkUserName = ({ target }) => {
+    const { name, type } = target;
+    const value = type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    }, () => {
+      const {
+        userInputName } = this.state;
+      const minAtrr = 3;
+      if (
+        userInputName.length >= minAtrr) {
+        this.setState({
+          isSaveButtonDisabled: false,
+        });
+      } else {
+        this.setState({
+          isSaveButtonDisabled: true,
+        });
+      }
+    });
+  };
+
   render() {
+    const { userInputName, isSaveButtonDisabled } = this.state;
     return (
       <>
-        <Route exact path="/" component={ Login } />
+        <Route exact path="/">
+          <Login
+            userInputName={ userInputName }
+            isSaveButtonDisabled={ isSaveButtonDisabled }
+            checkUserName={ this.checkUserName }
+          />
+        </Route>
         <Route exact path="/search" component={ Search } />
         <Route exact path="/album/:id" component={ Album } />
         <Route exact path="/favorites" component={ Favorites } />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
 
 export default class Login extends Component {
   render() {
+    const { userInputName, isSaveButtonDisabled, checkUserName } = this.props;
     return (
       <div data-testid="page-login">
         <Link to="/">Login</Link>
+        <label htmlFor="login-name-input">
+          <input
+            type="text"
+            data-testid="login-name-input"
+            id="login-name-input"
+            value={ userInputName }
+            name="userInputName"
+            onChange={ () => checkUserName }
+          />
+        </label>
+        <button
+          type="button"
+          disabled={ isSaveButtonDisabled }
+          id="loginbutton"
+          data-testid="login-submit-button"
+          onClick={ () => {
+            createUser({ name: `${userInputName}` });
+          } }
+        >
+          Entrar
+        </button>
+        { <p>Carregando...</p>}
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  userInputName: PropTypes.string.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  checkUserName: PropTypes.func.isRequired,
+};
