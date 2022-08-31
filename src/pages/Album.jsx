26a08ad51e165c2ae87bd6eb0c733c51d51d2941
Daniel Wolter <wolter.daniel@gmail.com>
2022-08-31 18:39:59 -0300

    req 7 em construçao

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
+import getMusics from '../services/musicsAPI';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
 
 export default class Album extends Component {
+  state = {
+    musicas: [],
+    favoritadas: [],
+    loading: false,
+  };
+
+  componentDidMount() {
+    const getListMusic = async () => {
+      const { match: { params: { id } } } = this.props;
+
+      const listMusics = await getMusics(id);
+
+      this.setState({
+        musicas: listMusics
+      });
+    };
+    getListMusic();
+  }
+
   render() {
     return (
       <div data-testid="page-album">
@@ -10,3 +31,11 @@ export default class Album extends Component {
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+}.isRequired;
